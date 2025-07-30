10b964ab65e44d14e35449a4e4b300ee771de382
Pramothsgp <jarvis.tech.006@gmail.com>
2025-07-30 17:58:54 +0000

    Update attendance routes to support new video + PKL object structure

--- server/src/routes/attendance.routes.ts
+++ server/src/routes/attendance.routes.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import { markAttendance, getAttendance } from '../controllers/attendance.controller';
+import { markAttendance, getAttendance, markAttendanceController } from '../controllers/attendance.controller';
 import { verifyAuth } from '../middleware/auth.middleware';
 import { upload } from '../middleware/multer.middleware';
-import { markAttendanceFromVideo } from '../services/attendance.service';
 const router = Router();
 
 router.post('/', verifyAuth, markAttendance);
 router.get('/:studentId', verifyAuth, getAttendance);
-router.post('/mark-attendance' , upload.array('videos') , markAttendanceFromVideo);
+router.post('/mark-attendance' , upload.array('videos') , markAttendanceController);
 export default router;
