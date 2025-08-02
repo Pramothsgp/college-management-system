5832eb0a46c8770c602f1e3ff09a313a302e9e25
Pramothsgp <jarvis.tech.006@gmail.com>
2025-08-02 15:45:55 +0000

    feat: Enhance attendance functionality with error handling and new date-based retrieval

--- server/src/controllers/attendance.controller.ts
+++ server/src/controllers/attendance.controller.ts
@@ -1,16 +1,39 @@
 import { Request, Response } from 'express';
-import { markAttendanceService, getAttendanceService, markAttendanceFromVideo } from '../services/attendance.service';
+import { markAttendanceService, getAttendanceService, markAttendanceFromVideo, getAttendanceOn } from '../services/attendance.service';
 
 export const markAttendance = async (req: Request, res: Response) => {
-  const data = await markAttendanceService(req.body);
-  res.json(data);
+  const { studentId, date, sessions } = req.body;
+  if (!studentId || !date || !sessions) {
+    return res.status(400).json({ error: 'Student ID, date, and sessions are required.' });
+  }
+  try {
+    const data = await markAttendanceService({ studentId, date, sessions });
+    res.json(data);
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 export const getAttendance = async (req: Request, res: Response) => {
-  const data = await getAttendanceService(req.params.studentId);
-  res.json(data);
+  try {
+    const data = await getAttendanceService(req.params.studentId);
+    res.json(data);
+  } catch (error: any) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
+export const getAttendanceByDate = async (req: Request, res: Response) => {
+  const date = req.query.date as string;
+  console.log('Fetching attendance for date:', date);
+  try{
+    const attendance = await getAttendanceOn(date);
+    res.json(attendance);
+  } catch (error: any) {
+    console.log('Error fetching attendance by date:', error);
+    res.status(400).json({ error: error.message });
+  }
+};
 
 export const markAttendanceController = async (req: Request, res: Response) => {
   try {
--- server/src/routes/attendance.routes.ts
+++ server/src/routes/attendance.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
-import { markAttendance, getAttendance, markAttendanceController } from '../controllers/attendance.controller';
+import { markAttendance, getAttendance, markAttendanceController, getAttendanceByDate } from '../controllers/attendance.controller';
 import { verifyAuth } from '../middleware/auth.middleware';
 import { upload } from '../middleware/multer.middleware';
 const router = Router();
 
 router.post('/', verifyAuth, markAttendance);
-router.get('/:studentId', verifyAuth, getAttendance);
+router.get('/student/:studentId', verifyAuth, getAttendance);
 router.post('/mark-attendance' ,verifyAuth, upload.array('videos') , markAttendanceController);
+router.get('/date', verifyAuth, getAttendanceByDate);
 export default router;
